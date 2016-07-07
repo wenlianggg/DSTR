7406d06bf8b88e8b2761c16645825ca99e732ea3
Wen Liang - Laptop <wenlianggg@gmail.com>
2016-07-07 13:41:04 +0800

    FixP8Q3

--- src/wl/p08/ParenthesisAppQ3.java
+++ src/wl/p08/ParenthesisAppQ3.java
@@ -4,33 +4,80 @@ import java.util.Scanner;
 import java.util.Stack;
 
 public class ParenthesisAppQ3 {
-	public static void main(String[] args) throws UnbalancedExpressionException {
-		Stack<Character> cs = new Stack<Character>();
-		Scanner expscan = new Scanner(System.in);
-		System.out.print("Enter expression: ");
-		char[] chararr = expscan.next().toCharArray();
-		
-		for (int i = 0; i < chararr.length; i++) {
-			System.out.print(chararr[i]);
-			if(chararr[i] == '(')
-				cs.push('(');
-			else if (chararr[i] == ')') {
-				if (cs.isEmpty()) {
-					System.out.println("Offending character: " + chararr[i]);
-				} else {
-					cs.pop();
-				}
-			} else if (chararr[i] == ' ') {
-				i++;
-			}
-		}
-		
-		expscan.close();
-		
-		if (cs.isEmpty()) {
-			System.out.println(cs.peek());
-			throw new UnbalancedExpressionException();
-		}
+		  public static void main(String[] args) {
+		        // TODO Auto-generated method stub
+		        /*
+		         * create an empty stack
+		            get first character of expression
+		            while not end of expression do
+		              if character is ‘(‘ then
+		                StackPush ‘(‘
+		              else 
+		                if character is ‘)’ then
+		               if IsStackEmpty = true then
+		                 error
+		               else 
+		                   StackPop
+		                       Get next character of expression
+		            endwhile
+		            if IsStackEmpty = false then
+		              error
+		         * 
+		         */ 
+		        Scanner sc = new Scanner(System.in);
+		        System.out.println("Enter an expression:");
+		        String exp = sc.nextLine();
+		        sc.close();
+		        Stack<String> stack = new Stack<String>();
+		        for (int i = 0; i < exp.length(); i++) 
+		        {
+		            String c = exp.substring(i, i+1);
+		            if (c.equals("(") || c.equals("[") || c.equals("{")) 
+		            {
+		                stack.push(c);
+		            } 
+		            else if (c.equals(")") || c.equals("]") || c.equals("}")) 
+		            {
+		                if (stack.isEmpty()) 
+		                {
+		                    System.out.println("Can't find a match for the '"+ c +"'.");
+		                    return;
+		                }
+		                else 
+		                {
+		                    String left = stack.pop();
+		                    if (!matching(c,left))
+		                    {
+		                        System.out.println("Can't find a match for the '"+ c +"'.");
+		                        return;
+		                    } else {} // nothing to do
+		                }
+		            }
+		            else { } // it's neither a ( nor a ), thus, nothing to do.
+		        }
+		        if (!stack.isEmpty())
+		        {
+		            String left = stack.peek();
+		            System.out.println("Can't find a match for the '" + left +"'.");            
+		        }
+		        return;
+		    }
+		    
+		    public static boolean matching(String right, String left) 
+		    {
+		        if ((left.equals("(")) && (right.equals(")")))
+		        {
+		            return true;
+		        }
+		        else if ((left.equals("[")) && (right.equals("]")))
+		        {
+		            return true;
+		        }
+		        else if ((left.equals("{")) && (right.equals("}")))
+		        {
+		            return true;
+		        }
+		        else { return false; }
+		    }
 		
 	}
-}
